a7b6a43db67eb00802e1ce84359e6d45495a4031
Joel Lacey <joellaceybusiness@gmail.com>
2020-02-12 10:02:03 -0700

    Add controlled form structure for Res input

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -5,15 +5,47 @@ export default class Form extends Component {
     constructor() {
         super()
         this.state = {
-
+            name: '',
+            date: '',
+            time: '',
+            guestCount: ''
         }
     }
 
+    updateResData = e => {
+        this.setState({[e.target.name] : e.target.value})
+    }
 
     render() {
         return (
-            <form className='form'>
-
+            <form className="form">
+                <input
+                    name="name"
+                    placeholder="Name"
+                    value={this.state.name}
+                    onChange={this.updateResData}
+                />
+                <input
+                    name="date"
+                    placeholder="Date (mm/dd)"
+                    value={this.state.date}
+                    onChange={this.updateResData}
+                />
+                <input
+                    name="time"
+                    placeholder="Time"
+                    value={this.state.time}
+                    onChange={this.updateResData}
+                />
+                <input
+                    name="guestCount"
+                    placeholder="Number of Guests"
+                    value={this.state.guestCount}
+                    onChange={this.updateResData}
+                />
+                <button onClick={this.submitRes}>
+                    Submit
+                </button>
             </form>
         )
     }
