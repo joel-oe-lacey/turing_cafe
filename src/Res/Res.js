0f53093b24ea7c9cecd7720266b6fb12695107e3
Joel Lacey <joellaceybusiness@gmail.com>
2020-02-12 09:34:11 -0700

    Build out Res card structure

--- src/Res/Res.js
+++ src/Res/Res.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Res.scss';
 
-const Res = () => {
-
+const Res = ({id, name, date, time, guestCount}) => {
     return (
-        <section className='reservation'>
-
+        <section key={id} className='reservation'>
+            <h3>{name}</h3>
+            <p>{date}</p>
+            <p>`${time} PM`</p>
+            <p>`Number of Guests: ${guestCount}`</p>
+            <button>Cancel</button>
         </section>
     )
 }
