17b0f1a337ea8e5a6f9fccd7c76d3cf570986df1
Joel Lacey <joellaceybusiness@gmail.com>
2020-02-12 10:13:13 -0700

    Fix styling and interpolation bugs

--- src/Res/Res.js
+++ src/Res/Res.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Res.scss';
 
-const Res = ({id, name, date, time, guestCount}) => {
+const Res = ({id, name, date, time, number}) => {
     return (
         <section key={id} className='reservation'>
             <h3>{name}</h3>
             <p>{date}</p>
-            <p>`${time} PM`</p>
-            <p>`Number of Guests: ${guestCount}`</p>
+            <p>{`${time} PM`}</p>
+            <p>{`Number of Guests: ${number}`}</p>
             <button>Cancel</button>
         </section>
     )
